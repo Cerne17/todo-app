86247896710c9af278d9276dd60ff2e3a0a35944
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-04-22 15:35:05 -0300

    fix[task]: updates endpoints

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import type { Task } from './task.entity';
 
@@ -6,28 +15,48 @@ import type { Task } from './task.entity';
 export class TaskController {
   constructor(private readonly taskService: TaskService) {}
 
-  @Get('/user/:id')
-  async findAllTasksByUserId(@Param('id') id: number) {
-    return this.taskService.findTasksByUserId(id);
+  // GET /tasks?userId=:userId&completed=:completed: returns all tasks for a user and filters by completion
+  @Get()
+  async findAll(@Query() query: any): Promise<Task[]> {
+    return this.taskService.findAll(query);
   }
 
+  // POST /tasks: creates a new task
   @Post()
-  async create(@Body() task: Task) {
+  async createTask(@Body() task: Task): Promise<Task> {
     return this.taskService.createTask(task);
   }
 
+  // GET /tasks/:id: returns a task by id
   @Get(':id')
-  async findOne(@Param('id') id: number) {
+  async findTaskById(@Param('id') id: number): Promise<Task> {
     return this.taskService.findTaskById(id);
   }
 
-  @Get('/user/:id/completed')
-  async findCompletedTasksByUserId(@Param('id') id: number) {
-    return this.taskService.findTaskByUserIdAndCompleted(id, true);
+  // PUT /tasks/:id: updates a task by id
+  @Put(':id')
+  async updateTask(
+    @Param('id') id: number,
+    @Body() updatedData: Partial<Task>,
+  ): Promise<Task> {
+    return this.taskService.updateTask(id, updatedData);
   }
 
-  @Get('/user/:id/uncompleted')
-  async findUncompletedTasksByUserId(@Param('id') id: number) {
-    return this.taskService.findTaskByUserIdAndCompleted(id, false);
+  // DELETE /tasks/:id: deletes a task by id
+  @Delete(':id')
+  async deleteTask(@Param('id') id: number): Promise<void> {
+    return this.taskService.deleteTask(id);
+  }
+
+  // POST /tasks/:id/complete: marks a task as completed
+  @Post(':id/complete')
+  async completeTask(@Param('id') id: number): Promise<Task> {
+    return this.taskService.completeTask(id);
+  }
+
+  // POST /tasks/:id/undo: marks a task as not completed
+  @Post(':id/undo')
+  async undoTask(@Param('id') id: number): Promise<Task> {
+    return this.taskService.undoTask(id);
   }
 }
