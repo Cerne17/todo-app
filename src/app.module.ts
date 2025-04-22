36898286737efa234a5fe47a1d018b76e77726c4
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-04-22 15:43:07 -0300

    feat[auth]: initializes the auth module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 import { TaskModule } from './task/task.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, TaskModule],
+  imports: [DatabaseModule, UserModule, TaskModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
