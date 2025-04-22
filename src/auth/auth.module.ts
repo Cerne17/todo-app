df3a2054fb69993828bacb034c91296cf1c3dc65
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2025-04-22 16:10:40 -0300

    feat[auth]: implements sign in

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserModule } from 'src/user/user.module';
+import { UserService } from 'src/user/user.service';
+import { userProviders } from 'src/user/user.provider';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [UserModule, DatabaseModule],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [...userProviders, AuthService, UserService],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async signIn(email: string, pw: string): Promise<any> {
+    const user = await this.userService.findUserByEmail(email);
+
+    if (user?.password !== pw) {
+      throw new UnauthorizedException();
+    }
+
+    const { password, ...result } = user;
+
+    // TODO: Generate JWT token here
+
+    return result;
+  }
+}
